b2b599dd3c2647a924fd8731f787071aad123671
VINTO1819 <develretr0@gmail.com>
2021-01-26 14:48:20 +0900

    Add Sptrite Component

--- src/Engine/Components/ArcadeSprite.ts
+++ src/Engine/Components/ArcadeSprite.ts
@@ -1,9 +1,25 @@
 import { ArcadeObject } from "../../Engine/Components/ArcadeObject.js"
 
+let NOTFOUND_ASSET = new Image()
+NOTFOUND_ASSET.src = "./../../../assets/NOTFOUND_ASSET.svg"
+
 export class ArcadeSprite extends ArcadeObject {
-  
-  constructor(){ 
+  private Texture:HTMLOrSVGImageElement = NOTFOUND_ASSET
+  private x:number 
+  private y:number
+  private TextureSizeWidth:number = this.Texture.width as number
+  private TextureSizeHeight:number = this.Texture.height as number
+
+  constructor(x:number | undefined, y:number | undefined, Texture:HTMLOrSVGImageElement | undefined){ 
     super() 
+    if(Texture != undefined){
+      this.Texture = Texture
+      this.TextureSizeWidth = this.Texture.width as number
+      this.TextureSizeHeight = this.Texture.height as number
+    }
+
+    this.x = x || (0 + this.TextureSizeWidth)
+    this.y = y || (0 + this.TextureSizeHeight)
   }
 
   ctx:CanvasRenderingContext2D | undefined
@@ -12,8 +28,50 @@ export class ArcadeSprite extends ArcadeObject {
     this.ctx = ctx
   }
 
+  private isMoving:boolean = false
+  private lastMills:number = Date.now()
+  private speed:number = 0
+  private moveTarget:SpritePoint = {x:-1, y:-1}
+  
   render(ctx: CanvasRenderingContext2D) {
-    
+    ctx.beginPath()
+    /*if(this.isMoving){
+      const timeMills = Date.now() - this.lastMills //지난 시간
+      const movedPixels = (timeMills / 1000) * this.speed
+      console.log(`${movedPixels}만큼 이동`)
+      this.x += (movedPixels)
+      this.y += (movedPixels)
+      
+    }*/
+
+    ctx.drawImage(this.Texture, this.x, this.y)
+    ctx.closePath()
+
+    this.lastMills = Date.now()
   }
 
+  moveTo(x:number, y:number, options:ArcadeSpriteOptions | undefined) {
+    if(options == undefined){
+      this.x = x
+      this.y = y
+    }else{
+      this.isMoving = true
+      this.speed = options.speed
+      this.moveTarget = {x, y}
+    }
+  }
+
+  setTexture(texture:HTMLOrSVGImageElement){
+    this.Texture = texture
+  }
+
+}
+
+interface SpritePoint{
+  x:number, y:number
+}
+
+
+export interface ArcadeSpriteOptions{
+  speed:number
 }
